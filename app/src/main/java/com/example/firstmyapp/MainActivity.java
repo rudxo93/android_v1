36ef8be63e701a549aeaea4ed6eafb4b9e40ae60
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-04-09 21:35:03 +0900

    Android 이해하기 18( constraintLayout을 이용해서 화면 만들어보기 )

--- app/src/main/java/com/example/firstmyapp/MainActivity.java
+++ app/src/main/java/com/example/firstmyapp/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mytube_layout_01);
+        setContentView(R.layout.constraint_02);
     }
 
 }
